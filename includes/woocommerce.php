d2c6f78856c083903b8d0df3a7b8594088cc19f7
rozklad <jan.rozklad@gmail.com>
2017-02-18 10:55:48 +0100

    merge

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -39,43 +39,33 @@ if (!function_exists('wyvern_wc_settings_footer'))
             }
         }
 
-    $user_id = get_current_user_id();
+        $user_id = get_current_user_id();
 
-    $wc_settings = [
-        // Woocommerce attributes (conditionally remove)
-        'attributes'         => woocommerce_get_all_attributes_with_values(),
+        $wc_settings = [
+            // Woocommerce attributes (conditionally remove)
+            'attributes'         => woocommerce_get_all_attributes_with_values(),
 
-        // Woocommerce cart
-        'cart'               => WC()->cart->get_cart(),
-        'cartTotal'          => $cart_total,
+            // Woocommerce cart
+            'cart'               => WC()->cart->get_cart(),
+            'cartTotal'          => $cart_total,
 
-        // Woocommerce settings
-        'gateways'           => WC()->payment_gateways->get_available_payment_gateways(),
-        'shipping'           => $shipping,
-        'decimal_separator'  => wc_get_price_decimal_separator(),
-        'thousand_separator' => wc_get_price_thousand_separator(),
-        'decimals'           => wc_get_price_decimals(),
-        'price_format'       => get_woocommerce_price_format(),
-        'currency'           => get_woocommerce_currency(),
-        'currency_symbol'    => get_woocommerce_currency_symbol(),
+            // Woocommerce settings
+            'gateways'           => WC()->payment_gateways->get_available_payment_gateways(),
+            'shipping'           => $shipping,
+            'decimal_separator'  => wc_get_price_decimal_separator(),
+            'thousand_separator' => wc_get_price_thousand_separator(),
+            'decimals'           => wc_get_price_decimals(),
+            'price_format'       => get_woocommerce_price_format(),
+            'currency'           => get_woocommerce_currency(),
+            'currency_symbol'    => get_woocommerce_currency_symbol(),
 
-        'customerId'         => get_current_user_id(),
+            'customerId'         => get_current_user_id(),
 
-        'wc_placeholder'     => wc_placeholder_img_src(),
+            'wc_placeholder'     => wc_placeholder_img_src(),
 
-        'wc_selected' => [
-            'shipping_methods' => WC()->session->get('wyvern_shipping_methods', []),
-            'payment_method'   => WC()->session->get('wyvern_payment_method'),
-        ],
-
-        'wc_user' => [
-            'billing' => [
-                'first_name' => get_user_meta($user_id, 'billing_first_name', true),
-                'last_name'  => get_user_meta($user_id, 'billing_last_name', true),
-                'address_1'  => get_user_meta($user_id, 'billing_address_1', true),
-                'address_2'  => get_user_meta($user_id, 'billing_address_2', true),
-                'phone'      => get_user_meta($user_id, 'billing_phone', true),
-                'email'      => get_user_meta($user_id, 'billing_email', true),
+            'wc_selected' => [
+                'shipping_methods' => WC()->session->get('wyvern_shipping_methods', []),
+                'payment_method'   => WC()->session->get('wyvern_payment_method'),
             ],
 
             'wc_user' => [
