1469f2451f7a69bbaf776cdaaa06af73ca7a3d30
rozklad <jan.rozklad@gmail.com>
2017-02-24 09:30:39 +0100

    menu in default

--- src/theme-header.vue
+++ src/theme-header.vue
@@ -7,18 +7,28 @@
         <div class="site-title">
             <router-link :to="{ path: wp.base_path }">{{ wp.site_name }}</router-link>
         </div>
+
+        <!-- Navigation -->
+        <ul class="nav">
+            <li v-for="item in menu">
+                <router-link :to="{ path: url2Slug(item.url) }">{{ item.title }}</router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
   mounted() {
-
+    this.getMenuLocation('primary', (response) => {
+      this.menu = response;
+    });
   },
 
   data() {
     return {
       wp: window.wp,
+      menu: [],
     };
   },
 
