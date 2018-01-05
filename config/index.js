349cc6b5d99558af14e6e13166637b00c2f3f28d
rozklad <jan.rozklad@gmail.com>
2018-01-05 14:06:47 +0100

    build path

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '../',
+    assetsPublicPath: './',
     productionSourceMap: true,
     devtool: '#source-map',
     // Gzip off by default as many popular static hosts such as
